8f951f9078fd6a9113ba8bda7a4cbc7c6a498890
mlyang1520 <36825518+mlyang1520@users.noreply.github.com>
2019-12-08 18:08:52 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1 class="text-center">About SummerSearch</h1>
-   <p class="text-center blockquote" > SummerSearch is a service seeking to democratize the education sector for high school students. Students, regardless of their backgrounds, are able to find the key experiences they need for growth. We hope that this free service can connect programs and individuals alike.</p>
+   <p class="text-center blockquote" > SummerSearch is a service seeking to democratize the education sector for high school students. Too often, the most enriching programs are accessible only to those students with the most wealth. Students, regardless of their backgrounds, should be able to find the key experiences in any subject they need for growth. We hope that this free service can connect programs and individuals alike.</p>
   <v-carousel cycle="cycle">
             <v-carousel-item src="http://engineering.nyu.edu/k12stem/stemnow/wp-content/uploads/2015/08/20150629_ARISE_Orientation042.jpg">
     </v-carousel-item>
@@ -13,10 +13,98 @@
         <v-carousel-item src="https://res.cloudinary.com/hud9ala09/image/upload/v1503440944/d6hbrirgeh1ov8thmsow.jpg">
     </v-carousel-item>
   </v-carousel>
+  <div> \n \n \n </div>
       <h1 class="text-center">Our Team</h1>
 
      <p class="text-center blockquote" > SummerSearch is a service seeking to democratize the education sector for high school students. Students, regardless of their backgrounds, are able to find the key experiences they need for growth. We hope that this free service can connect programs and individuals alike.</p>
 
+ <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col>
 
+ <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+                  <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/p960x960/67330186_2851590454870436_4572269548773834752_o.jpg?_nc_cat=103&_nc_ohc=HKfASMV3s78AQlW90ppQCIGU4-hEd6xRLYPLD3I0gYpCVZfEZeXvyN6iQ&_nc_ht=scontent-iad3-1.xx&oh=36cb5d56fd15b7c829d38b08387ba65c&oe=5E8561E4"
+                  >
+                  </v-img>
+                    <v-card-title>Lily Lou</v-card-title>
+
+                  <v-card-subtitle class="pb-0">Senior at UNC studying Computer Science and American Studies</v-card-subtitle>
+
+                  <v-card-text class="text--primary">
+
+                    <div>Lily enjoys---------</div>
+                  </v-card-text>
+                </v-card>        
+      </v-col>
+            <v-col>
+    <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+                  <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/60449624_2240366646293153_7002588222123409408_o.jpg?_nc_cat=108&_nc_ohc=MQcVRKfQg2QAQn537D1Wvy2a6T-v1q8TogNH9qgAW2BWQKSBNEQd_j_WA&_nc_ht=scontent-iad3-1.xx&oh=c3420373d42bd5fab8697499350ab896&oe=5E7B6C7A"
+                  >
+                  </v-img>
+                    <v-card-title primary-title>Cara Price</v-card-title>
+
+                  <v-card-subtitle class="pb-0">Senior at UNC studying Computer Science and History</v-card-subtitle>
+
+                  <v-card-text class="text--primary">
+
+                    <div>Cara enjoys---------</div>
+                  </v-card-text>
+                </v-card>        
+      </v-col>
+            <v-col>
+   <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+                  <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/11863416_839798632783499_6949904124110178605_n.jpg?_nc_cat=107&_nc_ohc=hcWdatV2uMMAQl-0EsNGXJ54TYgEQbeuVAQ2v-XeK21zOp7-twOrP38iQ&_nc_ht=scontent-iad3-1.xx&oh=4587f4fd9d4fc92d3c6bf45fcfcc5824&oe=5E73BEE4"
+                  >
+                  </v-img>
+                    <v-card-title>Evan Vitkus</v-card-title>
+                  <v-card-subtitle class="pb-0">Senior at UNC studying Computer Science and Statistics</v-card-subtitle>
+
+                  <v-card-text class="text--primary">
+
+                    <div>Evan enjoys playing video games (albeit poorly and often in a losing fashion)</div>
+                  </v-card-text>
+                </v-card>        
+      </v-col>
+            <v-col>
+   <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+                  <v-img
+                    class="white--text align-end"
+                    height="200px"
+                    src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/p960x960/54410680_3154649804561267_4774346316941623296_o.jpg?_nc_cat=103&_nc_ohc=fkrvJsN4yKgAQmVCJTUovee_sPTJ3YdMUvsRIzpU3bLnXIJsnoimV3yyQ&_nc_ht=scontent-iad3-1.xx&oh=bf6d9d720693570ba64ec0043320d4b6&oe=5E71D3EA"
+                  >
+                  </v-img>
+                    <v-card-title >Michael Yang</v-card-title>
+
+                  <v-card-subtitle class="pb-0">Senior at UNC studying Computer Science and History</v-card-subtitle>
+
+                  <v-card-text class="text--primary">
+
+                    <div>Michael enjoys playing video games (often winning)</div>
+                  </v-card-text>
+                </v-card>        
+      </v-col>
+    </v-row>
+  </v-container>
   </div>
 </template>
