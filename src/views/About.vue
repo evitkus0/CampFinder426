522943c30b6f9749ab4ac8c44a3198361f7615c2
mlyang1520 <36825518+mlyang1520@users.noreply.github.com>
2019-12-08 14:18:32 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-center">About SummerSearch</h1>
+   <p class="text-center blockquote" > SummerSearch is a service seeking to democratize the education sector for high school students. Students, regardless of their backgrounds, are able to find the key experiences they need for growth. We hope that this free service can connect programs and individuals alike.</p>
+  <v-carousel cycle="cycle">
+            <v-carousel-item src="http://engineering.nyu.edu/k12stem/stemnow/wp-content/uploads/2015/08/20150629_ARISE_Orientation042.jpg">
+    </v-carousel-item>
+    <v-carousel-item src="https://pbs.twimg.com/media/DYQXp2-X4AA5yFJ.jpg">
+
+    </v-carousel-item> 
+        <v-carousel-item src="https://exchanges.state.gov/files/exchanges/styles/exchange_program_banner/public/program-banners/940x282yes2.jpg?itok=Ize7tyID">
+    </v-carousel-item>
+        <v-carousel-item src="https://res.cloudinary.com/hud9ala09/image/upload/v1503440944/d6hbrirgeh1ov8thmsow.jpg">
+    </v-carousel-item>
+  </v-carousel>
+      <h1 class="text-center">Our Team</h1>
+
+     <p class="text-center blockquote" > SummerSearch is a service seeking to democratize the education sector for high school students. Students, regardless of their backgrounds, are able to find the key experiences they need for growth. We hope that this free service can connect programs and individuals alike.</p>
+
+
   </div>
 </template>
