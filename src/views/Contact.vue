70828cf9474f19e871d508241e27d3bf2ce3ccc9
evitkus0 <evitkus0@users.noreply.github.com>
2019-12-08 19:49:43 -0500

    Update Contact.vue
    Updated to reflect a final version

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="contact">
-    <h1>This is an contact page</h1>
+    <h1 class="text-center">Contact Us</h1>
+    <div class="entry-content" itemprop="text">
+      <h2 class="line-below text-center">Before you contact us, please take advantage of the frequently asked questions that are already available below.&nbsp; This detailed information will aid you in answering a multitude of questions.</h2>
+<p class="question">How can I find out more information about these camps?</p>
+<p class="answer">Our innovative card design allows you to click the camp name on our homepage to be redirected to their webpage!</p>
+<p class="question">Why are you doing this?</p>
+<p class="answer">During our high school careers we wanted to make the most of our time but didn't know how. In order to help todays youth, we compiled a list for you!</p>
+<p class="question">Is SummerSearch affiliated with the programs on their website?</p>
+<p class="answer">No, SummerSearch simply compiles third party programs to connect interested students.</p>
+<p class="question">Is SummerSearch meant for a specific demographic?</p>
+<p class="answer">SummerSearch is meant for an inclusive audience, and anyone within the high school age range can find uses for it.</p>
+<p class="question">Where do you find these programs?</p>
+<p class="answer">Our team has worked hard to compile many of these programs through their own experiences and research. Additionally, we receive information about programs from individuals looking to help!</p>
+<p class="question">Does SummerSearch cost money to use?</p>
+<p class="answer">No, SummerSearch will remain a free service--we only ask you to create accounts to help you track your favorite programs.</p>
+      <h4>
+        If you have additional questions, please contact us via email:
+        <a
+          href="mailto:summersearchercustomerservice@gmail.com"
+        >summersearchercustomerservice@gmail.com</a>
+      </h4>
+      <p>&nbsp;</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.question {
+  font-weight: bold;
+}
+.answer {
+  margin-left: 20px;
+}
+.line-below {
+  margin-bottom: 10px;
+}
+.contact {
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 10px;
+}
+</style>
