0dbc126932d2d50c04749d4569ed386f324d875f
evitkus0 <evitkus8@gmail.com>
2019-12-09 17:47:49 -0500

    Update Profile buttons

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,9 +5,24 @@
     <li>favorite 1</li>
     <li>favorite 2</li>
     <li>favorite 3</li>
-    <v-btn text v-on="on" v-on:click="isHidden = !isHidden" >View Account Preferences</v-btn>
-        <v-btn text v-on="on"  @click="submit" link to = "/">Delete Account</v-btn>
-<h1 v-if="!isHidden">Hide me on click event!</h1>
+    <div v-if="!isHidden">
+      <p>Hide me on click event!</p>
+      <!--<v-text-field
+        label="Name*"
+        v-model="$v.name.$model"
+        @input="$v.name.$touch()"
+        @blur="$v.name.$touch()"
+      ></v-text-field>-->
+      <v-text-field label="Name" v-model="name"></v-text-field>
+      <v-select :items="['9', '10', '11', '12']" label="Grade (Rising)" v-model="grade"></v-select>
+      <v-btn text v-on="on" v-on:click="isHidden = !isHidden">Submit</v-btn>
+      <v-btn v-if="!isHidden" text v-on="on" v-on:click="isHidden = !isHidden">Cancel</v-btn>
+    </div>
+    <v-btn v-if="isHidden" text v-on="on" v-on:click="isHidden = !isHidden">View Account Preferences</v-btn>
+    
+    <v-btn text v-on="on" @click="submit" link to="/">Delete Account</v-btn>
+    
+
   </div>
 </template>
 
@@ -26,13 +41,14 @@ export default {
   computed: {
     ...mapState(["authenticated"]),
     ...mapState(["user"]),
-    ...mapState(["name"])
+    ...mapState(["name"]),
+    ...mapState(["grade"])
   },
   methods: {
-      submit() {
-            this.$store.dispatch("deleteAccount");
-      }
+    submit() {
+      this.$store.dispatch("deleteAccount");
     }
+  }
 };
 </script>
 
