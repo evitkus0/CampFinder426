ec3257a0ffe9c7fa1888db9227794f699cf54f96
mlyang1520 <36825518+mlyang1520@users.noreply.github.com>
2019-12-09 15:32:13 -0500

    added route to account page

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,85 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Submit from "../views/Submit.vue";
-import Contact from "../views/Contact.vue";
+<template>
+  <span>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-Vue.use(VueRouter);
+        <v-list-item link to="/about">
+          <v-list-item-action>
+            <v-icon>mdi-white-balance-sunny</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
+        <v-list-item link to="/submit">
+          <v-list-item-action>
+            <v-icon>mdi-check-circle</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Submit</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link to="/contact">
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark clipped-left color="primary">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Summer Search</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span v-if="this.authenticated">
+        Hi {{name}}!
+        <v-btn text v-on="on" link to="/account">View Account Details</v-btn>
+      </span>
+      <span v-else>
+        <SignInDialog></SignInDialog>
+        <RegistrationDialog></RegistrationDialog>
+      </span>
+    </v-app-bar>
+  </span>
+</template>
+
+<script>
+import RegistrationDialog from "./RegistrationDialog.vue";
+import SignInDialog from "./SignInDialog.vue";
+
+import { mapState } from "vuex";
+
+export default {
+  name: "NavBar",
+  props: {
+    source: String
   },
-
-  {
-    path: "/about",
-    name: "about",
-    component: About
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    /*component: () =>
-      import("../views/About.vue")*/
+  components: {
+    RegistrationDialog,
+    SignInDialog
   },
-  {
-    path: "/submit",
-    name: "submit",
-    component: Submit
-  },
-  {
-    path: "/contact",
-    name: "contact",
-    component: Contact
-  },
-];
+  data: () => ({
+    drawer: null
+  }),
+  computed: {
+    ...mapState(["authenticated"]),
+    ...mapState(["user"]),
+    ...mapState(["name"])
+  }
+};
+</script>
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
-
-export default router;
+<style scoped></style>
