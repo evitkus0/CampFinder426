b690941b964dd233ce39bb552c116126cb178783
mlyang1520 <36825518+mlyang1520@users.noreply.github.com>
2019-12-09 17:03:38 -0500

    added delete

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,13 +5,15 @@
     <li>favorite 1</li>
     <li>favorite 2</li>
     <li>favorite 3</li>
-    <v-btn text v-on="on" link to="/account">View Account Preferences</v-btn>
+    <v-btn text v-on="on"  >View Account Preferences</v-btn>
+        <v-btn text v-on="on"   @click="submit" link to = "/">Delete Account</v-btn>
+
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-
+    import firebase from "firebase";
 export default {
   name: "NavBar",
   props: {
@@ -24,7 +26,13 @@ export default {
     ...mapState(["authenticated"]),
     ...mapState(["user"]),
     ...mapState(["name"])
-  }
+  },
+  methods: {
+      submit() {
+            firebase.auth().currentUser.delete();
+            console.log("asdfasdf")
+      }
+    }
 };
 </script>
 
