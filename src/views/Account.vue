c39b210680acf6bc46fa60f08fa517d26e39ead1
mlyang1520 <36825518+mlyang1520@users.noreply.github.com>
2019-12-09 22:09:16 -0500

    tried to add functions for changing name/grade

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -12,14 +12,16 @@
         @input="$v.name.$touch()"
         @blur="$v.name.$touch()"
       ></v-text-field>-->
-      <v-text-field label="Name" v-model="name"></v-text-field>
+      <v-text-field label="Name" required  v-model="name"    :error-messages="nameErrors"
+></v-text-field>
       <v-select :items="['9', '10', '11', '12']" label="Grade (Rising)" v-model="grade"></v-select>
-      <v-btn text v-on="on" v-on:click="isHidden = !isHidden">Submit</v-btn>
-      <v-btn v-if="!isHidden" text v-on="on" v-on:click="isHidden = !isHidden">Cancel</v-btn>
+      <v-btn text  v-on:click="isHidden = !isHidden" @click="submitEdits" 
+>Submit</v-btn>
+      <v-btn v-if="!isHidden" text  v-on:click="isHidden = !isHidden">Cancel</v-btn>
     </div>
-    <v-btn v-if="isHidden" text v-on="on" v-on:click="isHidden = !isHidden">View Account Preferences</v-btn>
+    <v-btn v-if="isHidden" text  v-on:click="isHidden = !isHidden">View Account Preferences</v-btn>
     
-    <v-btn text v-on="on" @click="submit" link to="/">Delete Account</v-btn>
+    <v-btn text  @click="deleteAccount" link to="/">Delete Account</v-btn>
     
 
   </div>
@@ -27,11 +29,14 @@
 
 <script>
 import { mapState } from "vuex";
-
+import required from "vuelidate/src/validators/required";
 export default {
-  name: "NavBar",
+  name: "Account",
   props: {
     source: String
+  },
+  validations:{
+    name: {required}
   },
   data: () => ({
     drawer: null,
@@ -41,15 +46,49 @@ export default {
     ...mapState(["authenticated"]),
     ...mapState(["user"]),
     ...mapState(["name"]),
-    ...mapState(["grade"])
+    nameErrors() {
+      const errors = [];
+      if (!name) return errors;
+      !name && errors.push("Name is required.");
+      return errors;
+    },
+    grade: {
+      get() {
+        return this.$store.state.grade;
+      },
+      set (value) {
+        //this.$store.commit('setGrade', grade)
+          this.$store.dispatch("updateUser", {
+          name: this.$store.name,
+          grade:this.$store.grade,
+        });
+      }
+    },
+        name: {
+      get() {
+        return this.$store.state.name;
+      },
+      set () {
+        //this.$store.commit('setGrade', grade)
+          this.$store.dispatch("updateUser", {
+          name: this.$store.name,
+          grade:this.$store.grade});
+      }
+    },
   },
   methods: {
-    submit() {
+    deleteAccount() {
       this.$store.dispatch("deleteAccount");
-    }
+    },
+    submitEdits(){
+
+        this.dialog = false;
+    } 
+
   }
 };
 </script>
 
 <style scoped></style>
+
 
